5939db3cd3e2ce1286cede9e1f200a21b1d5ac22
Erick CM <e.cardenasmendez@gmail.com>
2016-02-19 19:18:38 -0500

    Set main.go to check for Gitter token

--- cmd/supbot/main.go
+++ cmd/supbot/main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"os"
 
+	"github.com/gophergala2016/supbot/lib/gitter"
 	"github.com/gophergala2016/supbot/lib/slack"
 )
 
 func main() {
-	token := os.Getenv("SLACK_TOKEN")
-	if token == "" {
-		panic("slack token must be set")
+	slackToken := os.Getenv("SLACK_TOKEN")
+	gitterToken := os.Getenv("GITTER_TOKEN")
+	if slackToken == "" && gitterToken == "" {
+		panic("Slack or Gitter token must be set")
 	}
 
-	s := slack.NewClient(token)
-	s.Start()
+	if slackToken != "" {
+		s := slack.NewClient(slackToken)
+		s.Start()
+	} else if gitterToken != "" {
+		g, err := gitter.NewGitter(gitterToken)
+		if err != nil {
+			panic(err)
+		}
+
+		done := make(chan bool)
+		g.Start(done)
+		<-done // will wait forever
+	}
 }
